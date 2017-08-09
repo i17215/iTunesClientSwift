2a4532edba3527e4387c96172a61533488b1374b
Kirill <pshynia@icloud.com>
2017-08-09 23:54:17 +0400

    Initial Commit

---
+++ iTunesClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesClient
+//
+//  Created by Кирилл Колено on 09.08.17.
+//  Copyright © 2017 Treehouse Island. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
